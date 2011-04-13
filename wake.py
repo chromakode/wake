8b80a5072860816cf43a2c9df2590092f5701696
chromakode <c@chromakode.com>
2011-04-13 01:27:11 -0700

    Eh prints events and doesn't afraid of anything.

---
+++ wake.py
@@ -0,0 +1,15 @@
+import couchdb
+from been.couch import CouchStore
+
+from flask import Flask, render_template
+app = Flask(__name__)
+
+store = CouchStore()
+store.load()
+
+@app.route('/')
+def wake():
+    return render_template('listing.html', events=store.events())
+
+if __name__ == '__main__':
+    app.run(debug=True)
