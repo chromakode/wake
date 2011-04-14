a15811f170bd8c77cf18a6bfa3df89da16b9154a
Jesse Dubay <jesse@thefortytwo.net>
2011-04-13 22:05:19 -0700

    Add Jinja filters to prettify tweets and timestamps.

--- wake.py
+++ wake.py
@@ -1,9 +1,12 @@
 #!/usr/bin/env python
 from been.couch import CouchStore
 from flask import Flask, render_template
+from filters import relative_time, tweet
 
 app = Flask(__name__)
 app.jinja_env.trim_blocks = True
+app.jinja_env.filters["relativetime"] = relative_time
+app.jinja_env.filters["tweet"] = tweet
 
 store = CouchStore()
 store.load()
