9b8d18d52ef6ddd5009a448bcaf003435b387e72
Jesse Dubay <jesse@thefortytwo.net>
2011-04-17 15:09:50 -0700

    Add by_slug view for single events.

--- wake/views.py
+++ wake/views.py
@@ -1,5 +1,5 @@
 from been.couch import CouchStore
-from flask import render_template
+from flask import render_template, abort
 from wake import app
 
 store = CouchStore().load()
@@ -7,3 +7,11 @@ store = CouchStore().load()
 @app.route('/')
 def wake():
     return render_template('stream.html', events=store.collapsed_events())
+
+@app.route('/<slug>')
+def by_slug(slug):
+    events = list(store.events_by_slug(slug))
+    if not events:
+        abort(404)
+    return render_template('stream.html', events=events)
+
