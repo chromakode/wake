ee712beda0d0b2f7b3a546db117014b50b5942fe
chromakode <c@chromakode.com>
2011-04-20 04:24:39 -0700

    Linkify last.fm artists.

--- wake/__init__.py
+++ wake/__init__.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from urllib import quote_plus
 from wake.filters import relative_time, markup_tweet, markup_markdown
 
 app = Flask(__name__)
@@ -6,5 +7,6 @@ app.jinja_env.trim_blocks = True
 app.jinja_env.filters["relativetime"] = relative_time
 app.jinja_env.filters["tweet"] = markup_tweet
 app.jinja_env.filters["markdown"] = markup_markdown
+app.jinja_env.filters["quoteplus"] = quote_plus
 
 import wake.views
