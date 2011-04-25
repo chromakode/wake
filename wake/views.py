12acfff456e1a696d1117b20b8843c6789ee38bb
Jesse Dubay <jesse@thefortytwo.net>
2011-04-24 23:15:53 -0700

    Add Atom feed for events that have 'syndicate' set in their source config.

--- wake/views.py
+++ wake/views.py
@@ -1,5 +1,8 @@
 from been.couch import CouchStore
-from flask import render_template, abort
+from flask import render_template, abort, request, url_for
+from urlparse import urljoin
+from werkzeug.contrib.atom import AtomFeed
+from datetime import datetime
 from wake import app
 
 store = CouchStore().load()
@@ -15,3 +18,18 @@ def by_slug(slug):
         abort(404)
     return render_template('stream.html', events=events)
 
+@app.route('/recent.atom')
+def recent_feed():
+    feed = AtomFeed('Recent Posts', feed_url=request.url, url=request.url_root,
+                    generator=('Wake', None, None))
+    sources = store.get_sources()
+    for event in store.events():
+        if sources[event['source']].get('syndicate'):
+            feed.add(event['title'],
+                     unicode(event['content']),
+                     content_type='html',
+                     author=event.get('author', ''),
+                     url=urljoin(request.url_root, url_for('by_slug', slug=event.get('slug', ''))),
+                     updated=datetime.fromtimestamp(event['timestamp']),
+                     published=datetime.fromtimestamp(event['timestamp']))
+    return feed.get_response()
