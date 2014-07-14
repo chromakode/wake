c6ee59e33935edd357227640e301140be5873eed
Max Goodman <c@chromakode.com>
2014-07-14 16:26:36 -0700

    Add command-line arguments to runserver.py.

--- runserver.py
+++ runserver.py
@@ -1,5 +1,27 @@
 #!/usr/bin/env python
+import argparse
+
 from wake import app
 
 
-app.run(debug=True)
+parser = argparse.ArgumentParser()
+parser.add_argument(
+    '--host',
+    default='127.0.0.1',
+    help='hostname to listen on',
+)
+parser.add_argument(
+    '--port',
+    type=int,
+    default=5000,
+    help='port to listen on',
+)
+parser.add_argument(
+    '--debug',
+    type=bool,
+    default=True,
+    help='toggle tracebacks and debugger',
+)
+args = parser.parse_args()
+
+app.run(host=args.host, port=args.port, debug=app.debug)
